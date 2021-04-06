9a28eeefa3211488acf0f7af522dd5b8fef0cc38
Nikolowarrior <56362207+Nikolowarrior@users.noreply.github.com>
2021-04-06 18:39:05 -0400

    Segundo trabajo Ingenieria web

---
+++ submit.ts
@@ -0,0 +1,11 @@
+﻿function submit() {
+	params = new URLSearchParams(document.location.search.substring(1));
+	nombres = params.get("nombres");
+	if (nombres != null) {
+		formulario = document.getElementById("formulario");
+		formulario.style.visibility = "hidden";
+		formulario.style.height = "0px";
+		datosRecibidos = document.getElementById("datosRecibidos");
+		datosRecibidos.style.visibility="visible";
+	}
+}
